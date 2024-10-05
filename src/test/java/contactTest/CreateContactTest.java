69afc559bfa21625afd4f52df4a1c29e4e387c5f
irinr <irinr@IRINRAGHUL>
2024-10-05 13:34:32 +0530

    Drinking Partner

--- src/test/java/contactTest/CreateContactTest.java
+++ src/test/java/contactTest/CreateContactTest.java
@@ -3,6 +3,9 @@ package contactTest;
 public class CreateContactTest {
 	public static void main(String[] args) {
 		System.out.println("PIkachuh");
+		System.out.println("Got your project");
+		System.out.println("yo got hangover right??????????");
+		
 	}
 
 }
